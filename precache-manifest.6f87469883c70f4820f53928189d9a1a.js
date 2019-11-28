21f219d039fc1b763d4807f52be0362cae7a0314
Max <maxmeuer@me.com>
2019-11-28 11:01:37 +0100

    Updates

---
+++ precache-manifest.6f87469883c70f4820f53928189d9a1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3782dd1604bc9ff69a8f955eff8963b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8bd5735d54341175b9e1",
+    "url": "/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "dc49773a474cf525d199",
+    "url": "/static/css/main.b55cbedb.chunk.css"
+  },
+  {
+    "revision": "8bd5735d54341175b9e1",
+    "url": "/static/js/2.93c4f86a.chunk.js"
+  },
+  {
+    "revision": "dc49773a474cf525d199",
+    "url": "/static/js/main.ebe00554.chunk.js"
+  },
+  {
+    "revision": "daabeadd4ae2e929027d",
+    "url": "/static/js/runtime-main.1fe330ca.js"
+  }
+]);
